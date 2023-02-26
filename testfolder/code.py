3f5c6fc2abeb21c0b3c7dbaa9a2388b992f58b31
Oxana Mamchur <oxana.mamchur@gmail.com>
2023-02-26 14:59:32 +0100

    worked on style and layout

--- testfolder/code.py
+++ testfolder/code.py
@@ -3,20 +3,22 @@ from flask import Flask, request
 import sqlite3
 from datetime import datetime
 
+#configure application
 app = Flask(__name__)
 
+#make the route for a user submitting a newEntry
 @app.route("/newEntry", methods=["GET", "POST"])
 def submittedEntry():
-    if request.form.get("action") == "newEntrySubmitButton":
+    #if request.form.get("action") == "newEntrySubmitButton":
+    print("hello")
         
+
+#main route which shows the calendar with the current dates
 @app.route("/")
-#Calendar
+def showCalendar():
     #extracting current year and date
-
-current_datetime = datetime.now()
-current_month = current_datetime.month
-current_year = current_datetime.year
-
-print("hello world")
+    current_datetime = datetime.now()
+    current_month = current_datetime.month
+    current_year = current_datetime.year
 
 
